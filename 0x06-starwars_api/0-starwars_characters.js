0674f155b06cc5f6cfa197e23d6082aa9a006e92
guyestguygithub001 <danielene050@gmail.com>
2024-06-06 18:16:32 +0100

    genius

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,33 @@
 #!/usr/bin/node
 const request = require('request');
+const SWAPI_API = 'https://swapi-api.hbtn.io/api';
 
-function getCharacterName(url) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(JSON.parse(body).name);
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
+  const movieUrl = `${SWAPI_API}/films/${movieId}/`;
+
+  request(movieUrl, (error, _, body) => {
+    if (error) {
+      console.error(error);
+      return;
     }
+
+    const characterUrls = JSON.parse(body).characters;
+    const characterPromises = characterUrls.map(url =>
+      new Promise((resolve, reject) => {
+        request(url, (err, __, characterBody) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          const characterName = JSON.parse(characterBody).name;
+          resolve(characterName);
+        });
+      })
+    );
+
+    Promise.all(characterPromises)
+      .then(names => names.forEach(name => console.log(name)))
+      .catch(err => console.error(err));
   });
 }
-
-function getCharacters(movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const characters = JSON.parse(body).characters;
-      for (let i = 0; i < characters.length; i++) {
-        getCharacterName(characters[i]);
-      }
-    }
-  });
-}
-
-const movieId = process.argv[2];
-getCharacters(movieId);
